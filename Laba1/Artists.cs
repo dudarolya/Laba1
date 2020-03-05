b171965e8b17b3d9031607174495f9d1e378d6a8
olyaadudar@gmail.com <olyaadudar@gmail.com>
2020-03-05 21:14:04 +0200

    non-important changes

--- Laba1/Artists.cs
+++ Laba1/Artists.cs
@@ -6,13 +6,6 @@ namespace Laba1
 {
     public partial class Artists
     {
-        public int AId { get; set; }
-        [Display(Name = "Name")]
-        [Required(ErrorMessage = "This field can't be empty!")]
-        public string AName { get; set; }
-        [Display(Name = "Gender")]
-        [Required(ErrorMessage = "This field can't be empty!")]
-        public Genders AGender { get; set; }
         public enum Genders : int
         {
             Female,
@@ -23,10 +16,21 @@ namespace Laba1
             Transsexual,
             Other
         }
+        public int AId { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "This field can't be empty!")]
+        public string AName { get; set; }
+        [Display(Name = "Gender")]
+        [Required(ErrorMessage = "This field can't be empty!")]
+        [EnumDataType(typeof(Genders))]
+        public Genders AGender { get; set; }
+        
         [Display(Name = "Date of Birth")]
         [Required(ErrorMessage = "This field can't be empty!")]
         public DateTime ABirth { get; set; }
         [Display(Name = "Phone")]
+        [RegularExpression(@"\d{10}", ErrorMessage = "Invalid phone number!")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage ="Invalid number length! It has to be 10 nums")]
         public string APhone { get; set; }
         [Display(Name = "Group Name")]
         [Required(ErrorMessage = "This field can't be empty!")]
@@ -34,7 +38,6 @@ namespace Laba1
         [Display(Name = "Country Name")]
         [Required(ErrorMessage = "This field can't be empty!")]
         public int CountryId { get; set; }
-
         public virtual Countries Country { get; set; }
         public virtual Groups Group { get; set; }
     }
